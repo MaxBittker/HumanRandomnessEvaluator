9c9fb0b6525fc8761403a63cd70931ede0e1ed12
maxbittker <maxbittker@gmail.com>
2018-01-15 22:06:45 -0800

    explore simple response frequencies

--- src/hrand/core.clj
+++ src/hrand/core.clj
@@ -1,7 +1,37 @@
 (ns hrand.core
-  (:gen-class))
+  (:gen-class)
+  (require '[clojure.string :as str]))
+
+(def d (take 10000 (repeatedly (partial rand-int 2))))
+
+(def human (str/split "1000100010101010100101001010100010101010101010010111011010100010101110101101010100010111010011010101010010101001011101010" #""))
+
+(defn log2 [n] (/ (Math/log n) (Math/log 2)))
+
+(defn tuple-frequencies
+  [input n]
+  (vals (frequencies (partition n 1 input))))
+  
+(defn Hmax [l]
+  (log2 (Math/pow 2 l)))
+  
+(defn Hsingle [input]
+  (- (log2 (count input))
+    (/
+      (apply + 
+        (map 
+          (fn [n] (* n (log2 n)))
+          (tuple-frequencies input 2)))
+      (dec (count input)))))
+
+(defn R [input]
+  (* 100
+    (- 1
+      (/ (Hsingle input) (Hmax 2)))))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (println (R d)))
+
+
